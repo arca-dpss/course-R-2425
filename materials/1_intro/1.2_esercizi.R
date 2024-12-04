f696e1378eb90c733361d8003240d9ec31b1fb4c
Margherita Calderan <margherita.calderan@phd.unipd.it>
2024-12-04 19:13:43 +0100

    rename

---
+++ materials/1_intro/1.2_esercizi.R
@@ -0,0 +1,13 @@
+# Esercizi Lezione 1_intro (libro Introduction)
+
+# 1.1. Crea gli oggetti x e y e assegna loro il valore 22 e 4, rispettivamente. 
+
+# 1.2 Definisci una proposizione che ti permetta di valutare se la somma tra x e y 
+#    è un numero è pari. (tip: cosa ti ricorda %%?).
+
+# 4. Esegui le seguenti operazioni 4+3 | 5*2 >= 10,
+# Cosa osservi nell’ordine di esecuzione degli operatori?
+
+# 5. Esegui questo comando: 4+3 + (5*2 >= 10) e spiega il risultato
+
+#. 6 qualcosa con tante parentesi
---
+++ materials/2_struttureDati/2.4_esercizi.R
@@ -0,0 +1,43 @@
+# Esercizi data.frame
+
+# 1. Crea un dataframe così composto
+# -  Nome (fattore) contenente i nomi di voi 10 persone,
+# - Età (numerico) contenente la vostra età,
+# -  Altezza (numerico) contenente la vostra altezza in cm
+
+# 2. Estrai:
+# 2.1 Solo la colonna Nome (usando l'operatore $)
+# 2.2 La seconda colonna (usando l'indice).
+# 2.3 I valori di altezza per le persone con età minore a 24 anni.
+#     (utilizza sia funzione subset che [])
+
+# 3. Dato lo stesso data.frame dell'esercizio 2, 
+#  aggiungi una nuova colonna chiamata regione (inserisci la regione di nascita)
+
+# 4. Calcola la frequenza di ogni regione (?table)
+
+# 4. Seleziona le righe in cui l'altezza è minore o uguale di 1.60 kg e
+# e le colonne soprannome e età
+# utilizza sia funzione subset che []
+
+# 5. Crea un altro dataframe che abbia delle caratteristiche tali da poter 
+# essere unito al primo dataframe che hai creato (rbind())
+
+
+# 6. supponi di avere questi due dataframe
+# df1 <- data.frame(
+# ID = c(1, 2, 3),
+# Nome = c("Alice", "Bob", "Clara")
+# )
+# df2 <- data.frame(
+#   ID = c(2, 3, 4),
+#   Età = c(30, 22, 27)
+# )
+# Unisci i due data.frame utilizzando la colonna ID come chiave, (?merge)
+# mantenendo tutte le righe di entrambi i data.frame.
+
+
+# 7. Crea una terza colonna che unisca le informazioni nome e età (?paste, ?sprintf)
+
+
+# 8. Importazione dataframe
